bf036efcc50abc3389c613fe2bbd5cae092aaa0b
Đoàn Trí Hùng <koikoidth12@gmail.com>
2024-11-08 16:22:28 +0700

    .gitignore is now working

--- server/dist/utils/constant.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tokenLife = exports.protectedRoutes = exports.colorText = void 0;
-exports.colorText = "\x1b[36m%s\x1b[0m";
-exports.protectedRoutes = ["/api/users/profile"];
-exports.tokenLife = {
-    accessToken: "1h",
-    refreshToken: "7d",
-};
